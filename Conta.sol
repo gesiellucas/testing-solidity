44b2f2392d2ae234a84555f438f9015b06dc77bd
Gesiel Lucas Ferreira <32823495+gesiellucas@users.noreply.github.com>
2022-03-24 11:37:23 -0300

    feat: calculations in Solidity

---
+++ Conta.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.6.6;
+
+contract Conta {
+    uint Numero1;
+    uint Numero2;
+
+    function setNumero1(uint x) public {
+        Numero1 = x;
+    }
+
+    function setNumero2(uint y) public {
+        Numero2 = y;
+    }
+
+    function getNumero1() view public returns (uint) {
+        return Numero1;
+    }
+
+    function getNumero2() view public returns (uint) {
+        return Numero2;
+    }
+
+    function add() view public returns (uint) {
+        uint Sum = Numero1 + Numero2;
+        return Sum;
+    }
+
+    function sub() view public returns (uint) {
+        uint Sub = Numero1 - Numero2;
+        return Sub;
+    }
+
+    function mult() view public returns (uint) {
+        return Numero1 * Numero2;
+    }
+
+    function div() view public returns (uint) {
+        return Numero1 / Numero2;
+    }
+
+    
+    
+}
